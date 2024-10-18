6ebb64ae9acecaab1c9a3b4aec4c17c137126d81
SamuelVeytia <samveytia6@gmail.com>
2024-10-18 16:20:36 -0600

    Commit 3
    Se hizo una portada que sirve de inicio en el proyecto

---
+++ practicaRepasos/resources/views/portada.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite(['resources/js/app.js'])
+  <title>Portada</title>
+</head>
+<body style="background-color:#C70039">
+ 
+<div class="container mt-10 col-md-6">
+
+<div class="card" style="width: 18rem;">
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqKBX3eMPxlT3vFlKk3rjlB1VMKtT8OcZQ-A&s" class="card-img-top" alt="...">
+   <div class="card-body">
+    <h5 class="card-title">Práctica repaso</h5>
+    <h6 class="card-subtitle mb-2 text-body-secondary">Samuel Veytia López</h6>
+    <h6>Ingenieria en sistemas cumputacionales</h6>
+    <h6>Programación web</h6>
+  </div>
+
+
+</div>
+</div>
+  
+
+</body>
+</html>
