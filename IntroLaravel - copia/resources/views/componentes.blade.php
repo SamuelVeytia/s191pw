e10d36b3800334173872e1db546fbbf419f251be
SamuelVeytia <samveytia6@gmail.com>
2024-11-14 19:48:22 -0600

    Practica 8 avance

---
+++ IntroLaravel - copia/resources/views/componentes.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.plantilla1')
+
+@section('titulo','Componentes Blade')
+@section('contenido')
+
+    <x-Card encabezado="componente1" titulo="titulo1" textoBoton="Guardar"> 
+        Contenido de tarjeta 1
+    </x-Card>
+
+    <x-Card encabezado="componente2" titulo="titulo1" textoBoton="Guardar"> 
+        Contenido de tarjeta 2
+    </x-Card>
+
+    <x-Alert tipo="danger"> Rojo </x-Alert>
+    <x-Alert tipo="warning"> Amarillo </x-Alert>
+
+@endsection
